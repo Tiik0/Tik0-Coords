44b991c62d6fbf6de484d2eef22c3dc4f209e096
Tik0 <97636919+Tiik0@users.noreply.github.com>
2024-10-12 12:12:22 +0200

    Add files via upload

---
+++ Tik0_Coords/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'Tik00'
+
+version '1.0'
+
+lua54 'yes'
+
+ui_page 'html/index.html'
+
+client_scripts {
+	'client/main.lua'
+}
+
+files {
+    'html/index.html',
+    'html/index.js',
+    'html/index.css'
+}
